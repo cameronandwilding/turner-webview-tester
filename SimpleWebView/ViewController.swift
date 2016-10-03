deca69e1a66b6dea7250efe8ae602175fbb95c6a
Peter Arato <it.arato@gmail.com>
2016-10-03 12:08:05 +0100

    Initial Commit

---
+++ SimpleWebView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleWebView
+//
+//  Created by Peter Arato on 03/10/2016.
+//  Copyright © 2016 Peter Arato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
