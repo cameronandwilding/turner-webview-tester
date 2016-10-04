75280c27e555327955308d81bd9fb0104bdd4559
Peter Arato <it.arato@gmail.com>
2016-10-04 16:01:07 +0100

    Add files.

--- SimpleWebView/ViewController.swift
+++ SimpleWebView/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webview: UIWebView!
+    @IBOutlet weak var nidField: UITextField!
+    @IBOutlet weak var domainField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,17 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func onPressGo() {
+        let url_raw = "http://" + self.domainField.text! + "/editions/edition_JVuWy_2016-09-23/data/" + self.nidField.text! + "/index.html"
+        NSLog(url_raw)
+        let url = URL.init(string: url_raw)!
+        let request = URLRequest.init(url: url, cachePolicy: NSURLRequest.CachePolicy.reloadIgnoringLocalAndRemoteCacheData)
+        self.webview.loadRequest(request)
+    }
+    
+    @IBAction func onPressReload() {
+        self.webview.reload()
+    }
 
 }
-
